d3979e44de1b99bed7f84fab2df366c88d7eaa97
delvontedixon <delvontedixon@yahoo.com>
2024-01-30 12:04:03 -0500

    mentor session help

--- src/App.js
+++ src/App.js
@@ -1,12 +1,106 @@
-import logo from "./logo.svg";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const COMMENTS_ENDPOINT = "";
+  // NOTE GET request from the API or local json file
+  const getComments = async () => {
+    console.log("getting comment data from mockapi...");
+
+    try {
+      const response = await fetch(COMMENTS_ENDPOINT); // url you want get data from
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const resp = await response.json(); // parsing the response as JSON
+      return resp;
+    } catch (error) {
+      console.log(
+        "Oh no! There was an error with getting your comment.",
+        error
+      );
+    }
+  };
+
+  // NOTE POST requests from the API or local json file
+  const postComment = async (commentData) => {
+    console.log("posting comment", commentData);
+    try {
+      const response = await fetch(COMMENTS_ENDPOINT, {
+        // url you want to post to
+        method: "POST", // type of request
+        headers: {
+          "Content-Type": "application/json", // type of data sent to the server
+        },
+        body: JSON.stringify({ commentName: commentData }), // data you want to update
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const resp = await response.json(); // parsing the response as JSON
+      return resp;
+    } catch (error) {
+      console.log(
+        "Oh no! There was an error with posting your comment.",
+        error
+      );
+    }
+  };
+
+  // NOTE PUT request from the API or local json file
+  const updateComment = async (commentData, commentId) => {
+    console.log("updating comment", commentData);
+    try {
+      const response = await fetch(COMMENTS_ENDPOINT + `/${commentId}`, {
+        // url + id you want to update
+        method: "PUT", // type of request
+        headers: {
+          "Content-Type": "application/json", // type of data sent to the server
+        },
+        body: JSON.stringify({ commentName: commentData }), // data you want to update
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const resp = await response.json(); // parsing the response as JSON
+      return resp;
+    } catch (error) {
+      console.log(
+        "Oh no! There was an error with updating your comment.",
+        error
+      );
+    }
+  };
+
+  // NOTE DELETE request from the API or local json file
+  const deleteComment = async (commentId) => {
+    console.log("deleting comment", commentId);
+    try {
+      const response = await fetch(COMMENTS_ENDPOINT + `/${commentId}`, {
+        // url + id you want to delete
+        method: "DELETE", // type of request
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response; // The DELETE request might not return a JSON response
+    } catch (error) {
+      console.log(
+        "Oh no! There was an error with deleting your comment.",
+        error
+      );
+    }
+  };
   return (
     <div className="App">
       <h1>Testing App</h1>
+      <Button>Button example</Button>
     </div>
   );
 }
 
 export default App;
+
+// Conts url instead of static url
+// https://65b923cbb71048505a8a4706.mockapi.io/teams
