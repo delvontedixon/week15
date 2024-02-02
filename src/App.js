732f42036d475b94c7ba3cfd143e377b60140f18
delvontedixon <delvontedixon@yahoo.com>
2024-02-02 15:30:59 -0500

    Fixing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import React from 'react';
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./App.css";
-import Button from "react-bootstrap/Button";
 import Teams from "./components/teams";
+import { Link } from "react-router-dom";
+
+import default function Navigate(){
+  return (
+      <nav class="flex items-center justify-between flex-wrap bg-green-500 p-6">
+      <div class="flex items-center flex-shrink-0 text-white mr-6">
+          <span class="font-semibold text-xl tracking-tight">REACT CRUD APP</span>
+      </div>
+      <Link to="/">
+          <button class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white mt-4 lg:mt-0">
+              HOME
+          </button>
+      </Link>
+      </nav>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Testing App</h1>
-      <Button>Button example</Button>
       <br />
       <Teams />
     </div>
